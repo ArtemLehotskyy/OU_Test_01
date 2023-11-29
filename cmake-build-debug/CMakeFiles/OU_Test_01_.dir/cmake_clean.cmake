13ce664fcba9f9f56d5dffca7487774d6096d01f
ArtemLehotskyy <252810@edu.p.lodz.pl>
2023-11-29 15:25:31 +0100

    Nowa metoda Policz

---
+++ cmake-build-debug/CMakeFiles/OU_Test_01_.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Test_01_.dir/main.cpp.obj"
+  "CMakeFiles/OU_Test_01_.dir/main.cpp.obj.d"
+  "OU_Test_01_.exe"
+  "OU_Test_01_.exe.manifest"
+  "OU_Test_01_.pdb"
+  "libOU_Test_01_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_Test_01_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_Test_01_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_Test_01_.
+# This may be replaced when dependencies are built.
